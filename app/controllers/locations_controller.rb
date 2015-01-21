9b80ffa40aee20e391bf42527700dee3a38c6b84
Eric Kenney <ekenney5@gmail.com>
2015-01-20 21:25:42 -0700

    Checkboxes remain after search

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,11 +1,17 @@
 class LocationsController < ApplicationController
   def index
+    @features = Feature.all.select(:name)
     @locations = current_user.locations
     @center = Location.find_center(@locations)
   end
 
   def search
-    @locations = Location.joins(:features).where(features: {name: params[:features]} )
+    @features = params[:features].nil? ? Feature.all.select(:name) : params[:features]
+    @locations = Location.joins(:features).where(features: {name: @features} )
+    if @locations.empty?
+      @locations = current_user.locations
+      flash.now[:notices] = ["No locations matched filters"]
+    end
     @center = Location.find_center(@locations)
     render :index
   end
