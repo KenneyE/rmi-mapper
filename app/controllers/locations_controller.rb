799eeb4c6ba1429950572090a69a851dd64b86c4
Eric Kenney <ekenney5@gmail.com>
2015-02-08 15:40:41 -0700

    Added ability to create new locations

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,4 +1,17 @@
 class LocationsController < ApplicationController
+
+  def create
+    location = Location.new(location_params)
+    if location.save!
+      location.user_locations.create(user_id: current_user.id, location_id: location.id)
+      flash[:notices] = ["Location '#{location.name}' successfully created!"]
+      redirect_to current_user
+    else
+      flash.now[:errors] = location.errors.full_messages
+      render current_user
+    end
+  end
+
   def index
     @features = Feature.all.select(:name)
     @locations = current_user.locations
@@ -14,5 +27,10 @@ class LocationsController < ApplicationController
     end
     @center = Location.find_center(@locations)
     render :index
+  end
+
+  private
+  def location_params
+    params.require(:location).permit(:name, :lat, :lon, :location_type)
   end
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,10 +1,21 @@
 class Location < ActiveRecord::Base
   validates :name, presence: true
+  validates :location_type, :inclusion => { :in => ['fixed', 'ship'] }
+
+  before_validation :set_location_type
 
   has_many :user_locations, dependent: :destroy
   has_many :users, through: :user_locations
 
   def self.find_center(locations)
     {lat: locations.average("lat"), lon: locations.average("lon")}
+  end
+
+  def set_location_type
+    if self.location_type == '0'
+      self.location_type = 'fixed'
+    elsif self.location_type == '1'
+      self.location_type = 'ship'
+    end
   end
 end
