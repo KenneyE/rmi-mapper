f93a5fde405ec497076f0cfedfedb787ad41310e
Eric Kenney <ekenney5@gmail.com>
2015-02-08 22:18:02 -0700

    Adding marine traffic api

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -29,6 +29,10 @@ class LocationsController < ApplicationController
     render :index
   end
 
+  def update_coords
+
+  end
+
   private
   def location_params
     params.require(:location).permit(:name, :lat, :lon, :location_type)
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     root to: "users#show", as: "user"
     get '/admins/new', to: "users#new_admin", as: :new_admin
     post '/admins', to: "users#create_admin", as: :create_admin
+    post '/locations/:id/edit/:lat/:lon', to: "locations#update_coords", as: :update_location_coords
     resources :locations, shallow: true
   end
 
