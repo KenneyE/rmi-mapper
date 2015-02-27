878ef599e45dbba1180f56887c73c57a402972c6
Eric Kenney <ekenney5@gmail.com>
2015-02-26 21:45:34 -0700

    Limited range of hospitals to 15 degrees

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -8,20 +8,19 @@ class Hospital < ActiveRecord::Base
     {lat: locations.average("lat"), lon: locations.average("lon")}
   end
 
-  def self.find_nearby_with_features(lat, lon, searched_features, max_dist = 45)
+  def self.find_nearby_with_features(lat, lon, searched_features, max_dist = 15)
     lat, lon = lat.to_f, lon.to_f
 
     if searched_features.empty?
-      # hospitals = Hospital.where(lat: (lat - max_dist)..(lat + max_dist))
-      #   .where(lon: (lon-max_dist)..(lon + max_dist))
-        hospitals = Hospital.all
+      hospitals = Hospital.where(lat: (lat - max_dist)..(lat + max_dist))
+        .where(lon: (lon-max_dist)..(lon + max_dist))
     else
       hospitals = Hospital.joins(:features)
         .where('features.id in (?)', searched_features)
         .group("hospitals.id")
         .having('COUNT(*) = ?', searched_features.length)
-        # .where(lat: (lat - max_dist)..(lat + max_dist))
-        # .where(lon: (lon-max_dist)..(lon + max_dist))
+        .where(lat: (lat - max_dist)..(lat + max_dist))
+        .where(lon: (lon - max_dist)..(lon + max_dist))
     end
   end
 
