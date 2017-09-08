0a73a2eba457735dac43e90b69d429832d92e437
Eric Kenney <ekenney5@gmail.com>
2017-09-07 21:15:55 -0600

    Add guest user and login

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+guest = User.create!(email: "guest@example.com", password: "password123")
 user = User.create!(email: "eric@test.com", password: "foobar11")
 admin = User.create!(email: "admin@test.com", password: "foobar11", admin: true)
 
@@ -18,35 +19,39 @@ end
 
 400.times do |i|
   hosp = Hospital.create(name: "Hospital ##{i}",
-    lat: (-80 + 160 * rand()).round(7),
-    lon: (-179 + 358 * rand()).round(7))
+  lat: (-80 + 160 * rand()).round(7),
+  lon: (-179 + 358 * rand()).round(7))
 
-    HospitalFeature.create(hospital_id: hosp.id, feature_id: feature_ids.sample)
-    HospitalFeature.create(hospital_id: hosp.id, feature_id: feature_ids.sample)
-    HospitalFeature.create(hospital_id: hosp.id, feature_id: feature_ids.sample)
+  HospitalFeature.create(hospital_id: hosp.id, feature_id: feature_ids.sample)
+  HospitalFeature.create(hospital_id: hosp.id, feature_id: feature_ids.sample)
+  HospitalFeature.create(hospital_id: hosp.id, feature_id: feature_ids.sample)
 
-  # UserLocation.create(location_id: loc.id, user_id: user.id)
-end
-
-5.times do |i|
-  loc = user.locations.create(name: "Fixed Location ##{i}",
-    lat: (10 + 50 * rand()).round(7),
-    lon: -(80 + 60 * rand()).round(7),
-    location_type: "fixed")
-
-    UserLocation.create(user: admin, location: loc)
   # UserLocation.create(location_id: loc.id, user_id: user.id)
 end
 
 mmsi_vals = [511097000, 265735000, 366709770, 367381230, 211505620]
 ship_names = ["JAMILEH", "MINI STAR", "WSF CHELAN", "MATTHEW MCALLISTER", "WESERLAND"]
 mmsi_vals.each_with_index do |mmsi, i|
-  loc = user.locations.create(
-    name: ship_names[i],
-    lat: (10 + 50 * rand()).round(7),
-    lon: -(80 + 60 * rand()).round(7),
-    location_type: "ship",
-    mmsi: mmsi)
+  loc = Location.create(
+  name: ship_names[i],
+  lat: (10 + 50 * rand()).round(7),
+  lon: -(80 + 60 * rand()).round(7),
+  location_type: "ship",
+  mmsi: mmsi)
 
-    UserLocation.create(user: admin, location: loc)
+  UserLocation.create(user: user, location: loc)
+  UserLocation.create(user: admin, location: loc)
+  UserLocation.create(user: guest, location: loc)
+end
+
+5.times do |i|
+  loc = Location.create(name: "Fixed Location ##{i}",
+  lat: (10 + 50 * rand()).round(7),
+  lon: -(80 + 60 * rand()).round(7),
+  location_type: "fixed")
+
+  UserLocation.create(user: user, location: loc)
+  UserLocation.create(user: admin, location: loc)
+  UserLocation.create(user: guest, location: loc)
+  # UserLocation.create(location_id: loc.id, user_id: user.id)
 end
